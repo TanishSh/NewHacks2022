fb0bb4e78ea2bde100d5caa52034715f1b94a553
Tanish Sharma <tanishsharma.krish@gmail.com>
2022-11-06 01:20:29 -0500

    firebase works to upload pics

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import csv
+from firebase_admin import credentials, initialize_app, storage
 
 time = []
 bpm = []
@@ -15,4 +16,21 @@ plt.xticks(rotation = 25)
 plt.xlabel('Time (mms)')
 plt.ylabel('Heart Rate (bpm)')
 plt.title('Analyzing Health conditions, Mood, and Sleeping Patterns from Heart Beats', fontsize = 17)
-plt.show()
+#plt.show()
+plt.savefig("graph.jpg")
+
+
+# Init firebase with your credentials
+cred = credentials.Certificate("./google.json")
+initialize_app(cred, {'storageBucket': 'ecaff-367805.appspot.com'})
+
+# Put your local file path 
+fileName = "./graph.jpg"
+bucket = storage.bucket()
+blob = bucket.blob(fileName)
+blob.upload_from_filename(fileName)
+
+# Opt : if you want to make public access from the URL
+blob.make_public()
+
+print("your file url", blob.public_url)
